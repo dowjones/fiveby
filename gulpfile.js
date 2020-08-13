1cfd97af756dbd7167cb5e2eddd20da9d2f5b22d
Scott Rahner <scott.rahner@gmail.com>
2020-08-13 13:42:20 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,8 @@ var mocha = require('gulp-mocha');
 var jscs = require('gulp-jscs');
 var jshint = require('gulp-jshint');
 var print = require('gulp-print');
-
-gulp.task('test', ['style'], function () {
-  gulp.src(['lib/*.js', 'index.js'])
+function test() {
+  return gulp.src(['lib/*.js', 'index.js'])
     .pipe(istanbul())
     .pipe(istanbul.hookRequire())
     .on('finish', function () {
@@ -25,22 +24,23 @@ gulp.task('test', ['style'], function () {
           }
         });
     });
-});
-
-gulp.task('func', function () {
-  gulp.src(['test/func/*.js'])
-    .pipe(smocha({timeout: 30000, slow: 15000, 'delay': true}));
-});
-
-gulp.task('debug', function () {
-  gulp.src(['test/spec/*.js'])
-    .pipe(mocha());
-});
-
-gulp.task('style', function () {
-  gulp.src(['lib/*.js','test/**/*.js','index.js'])
+}
+//
+// gulp.task('func', function () {
+//   gulp.src(['test/func/*.js'])
+//     .pipe(smocha({timeout: 30000, slow: 15000, 'delay': true}));
+// });
+//
+// gulp.task('debug', function () {
+//   gulp.src(['test/spec/*.js'])
+//     .pipe(mocha());
+// });
+function style() {
+  return gulp.src(['lib/*.js','test/**/*.js','index.js'])
     .pipe(jscs())
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'))
     .pipe(jshint.reporter('fail'));
-});
+}
+gulp.task('test', gulp.series(style, test));
+gulp.task('style', style);
